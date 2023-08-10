29233d725fdcb0b5380af5d8ebd9b640a6c5e2a9
agungardneham <agungmahendra1305@gmail.com>
2023-08-11 03:49:15 +0700

    add more to profile details

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,33 @@ module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      raleway: ["Raleway", "sans-serif"],
+      pacifico: ["Pacifico", "cursive"],
+    },
+    colors: {
+      primary: "#F2F5F9",
+      secondary: "#FF9C1A",
+      tertiary: "#EC1B09",
+    },
+    boxShadow: {
+      custom: "0px 0px 0px 0px rgba(0, 0, 0, 0.25)",
+      custom2: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".filter": {
+          filter: "var(--tw-filter)",
+        },
+        ".drop-shadow-0-0-1-rgba-0": {
+          "--tw-filter": "drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.00))",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };

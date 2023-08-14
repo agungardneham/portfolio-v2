5bec99c601f288ffcdb1c83b604bb40b73433ac0
agungardneham <agungmahendra1305@gmail.com>
2023-08-14 09:40:02 +0700

    add hoverability and desktop responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
     boxShadow: {
       custom: "0px 0px 0px 0px rgba(0, 0, 0, 0.25)",
       custom2: "0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
+      custom3: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
     },
     borderRadius: {
       custom: "0rem 0rem 0.5rem 0.5rem",
